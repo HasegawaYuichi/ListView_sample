fc6dab69d9b966b2bae2a83d00afdd9da56030ff
Hasegawa <hasegawa.yuichi@kk.jp.panasonic.com>
2022-10-01 23:09:53 +0900

    既存リスト追加、編集ボタン削除

--- app/src/main/java/com/example/listview_sample/MainActivity.kt
+++ app/src/main/java/com/example/listview_sample/MainActivity.kt
@@ -1,33 +1,37 @@
 package com.example.listview_sample
 
-//import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.EditText
-import android.widget.ListView
 import androidx.appcompat.app.AlertDialog
+import com.example.listview_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        //viewの取得
-        val lv:ListView = findViewById(R.id.lv)
-        //ListViewにリストを追加するためのボタン
-        val btnAdd:Button = findViewById(R.id.btnAdd)
-        //選択モードで選択したリストを削除するためのボタン
-        val deletebtn:com.google.android.material.floatingactionbutton.FloatingActionButton = findViewById(R.id.deleteBtn)
-        //ListViewのリストを選択モードに移行するためのボタンとして用意
-        val btnEdit:Button = findViewById(R.id.btnEdit)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val lv = binding.lv
+        val btnAdd = binding.btnAdd
+        val deletebtn = binding.deleteBtn
+
+        // データを用意
+        val data = mutableListOf(
+            "a", "b", "c", "d",
+            "e", "f", "g", "h"
+        )
 
         //アダプターに入れてListViewをセット
-        val adapter = ArrayAdapter<String>(
+        val adapter = ArrayAdapter(
             this,
             android.R.layout.simple_list_item_1,
-            arrayListOf()
+            data
         )
         lv.adapter = adapter
 
@@ -37,12 +41,11 @@ class MainActivity : AppCompatActivity() {
             val et = EditText(this)
             AlertDialog.Builder(this)
                 .setTitle("項目の追加")
-                .setMessage("何をする？")
                 .setView(et)
                 .setPositiveButton("追加"){ _, _ ->
                     //add()でアダプターに追加する
-                    val myTodo = et.text.toString()
-                    adapter.add(myTodo)
+                    val adlist = et.text.toString()
+                    adapter.add(adlist)
                 }
                 .setNegativeButton("取消",null)
                 .show()
@@ -52,19 +55,19 @@ class MainActivity : AppCompatActivity() {
         deletebtn.setOnClickListener{
             AlertDialog.Builder(this)
                 .setTitle("項目を削除しますか？")
-                //Yesを押したらremoveで削除
+                //Yesを押したらremoveで削除、未実装部なのでnull
                 .setPositiveButton("Yes",null)
                 .setNegativeButton("No",null)
                 .show()
         }
 
-        //ListViewをタッチしたらアラートダイアログ
+        //ListViewをタッチしたらアラートダイアログ表示
         lv.setOnItemClickListener { _, _, i, _ ->
             AlertDialog.Builder(this)
                 .setTitle("項目を削除しますか？")
-                //Yesを押したらremoveで削除
+                //Yesを押したらremoveでリストを削除
                 .setPositiveButton("Yes") { _, _ ->
-                    adapter.remove(adapter.getItem(i))
+                    adapter.remove(data[i])
                     adapter.notifyDataSetChanged() //画面更新
                 }
                 .setNegativeButton("No",null)
